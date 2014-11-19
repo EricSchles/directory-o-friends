c0df453470fbb2340814b6c32fce902d9f2cb0d5
EricSchles <ericschles@gmail.com>
2014-11-19 06:02:39 -0800

    updated config

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
 from views import app
 
+app.config["DATABASE"] = 'database.db'
+app.config['DEBUG'] = True
+app.config["SECRET_KEY"] = "like I'd tell you"
+app.config["USERNAME"] = "Eric"
+app.config["PASSWORD"] = "Schles"
 app.run(debug=True)
